5a04803ce50a8d4b2703523a3708b0b2cd5a2468
shannone <shannone426@gmail.com>
2022-08-05 11:27:02 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Code your solution in this file!
-// did my repo work lol
+
+
+
+// set companyNmae to = "scuber"
+const companyName = "Scuber";
+
+// set mostProfitableNeighborhood to Chelsea
+let mostProfitableNeighborhood = "Chelsea";
+
+// set companyCeo to Susan Smith
+let companyCeo = "Susan Smith";
